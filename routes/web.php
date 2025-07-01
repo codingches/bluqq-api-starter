cfaf7812cf8fe92d0c58e800843726ea69020598
COding-guru's <ahiabuikeexcel@gmail.com>
2025-07-01 15:08:31 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/migrate-now', function () {
+    \Artisan::call('migrate', ['--force' => true]);
+    return 'Migration complete!';
+});
